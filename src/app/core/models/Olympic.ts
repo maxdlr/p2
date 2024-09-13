e4faa78eb1f7acaf1dad56f89dafe89c1fc830e7
maxdlr <contact@maxdlr.com>
2024-09-13 14:56:34 +0200

    refine models and type check http request

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -8,14 +8,18 @@ example of an olympic country:
 }
 */
 
+import {Participation} from './Participation';
+
 export class Olympic {
   public id!: number;
   public country!: string;
   public participations!: Participation[];
 
-  constructor(id: number, country: string, participations: Participation[]) {
-    this.id = id;
-    this.country = country;
-    this.participations = participations;
+  public getOlympic() {
+    return {
+      id: this.id,
+      country: this.country,
+      participations: this.participations,
+    };
   }
 }
